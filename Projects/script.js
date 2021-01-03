be5ade37ff729e74be35da706d8d6c5556b6c2ff
zanbonzakuro <gaybullaev993@gmail.com>
2021-01-03 11:54:06 +0500

    first commit

---
+++ Projects/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//alert("Hello");
+//const result = confirm ("Are you here?");
+//console.log(result);
+
+//const answer = prompt("вам есть 18?", "18");
+//console.log(typeof(answer));
+
+//const answers = [];
+
+//answers[0] = prompt('Сколько вам лет?', '');
+//answers[1] = prompt ('Как вас зовут?', '');
+//[2] = prompt ('как ваша фамилия?', '');
+
+//document.write(answers);
+
+const category = 'toys';
+
+console.log('https://someurl.com/' + category +'/'+'4');
--- script.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-let number = 5;
-const leftBorderWidth = 1;
-let fuckYou = "alex you are fool";
-console.log(fuckYou);
